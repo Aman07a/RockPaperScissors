52de85c9ea7826c6da523cb69f1ca01a3f9bcf5a
Aman Ahmed <aman16.aa17@gmail.com>
2023-03-26 17:42:43 +0200

    Worked Example: Rock, Paper, Scissors Extension

--- rock-paper-scissors.ts
+++ rock-paper-scissors.ts
@@ -12,12 +12,47 @@ interface RockPaperScissors {
   play(playerMove: Move, opponentMove: Move): Outcome;
 }
 export function createRockPaperScissors(): RockPaperScissors {
+  const scenarios = new Array(
+    {
+      playerMove: Move.Scissors,
+      opponentMove: Move.Paper,
+      outcome: Outcome.PlayerWins,
+    },
+    {
+      playerMove: Move.Scissors,
+      opponentMove: Move.Rock,
+      outcome: Outcome.PlayerLoses,
+    },
+    {
+      playerMove: Move.Rock,
+      opponentMove: Move.Scissors,
+      outcome: Outcome.PlayerWins,
+    },
+    {
+      playerMove: Move.Rock,
+      opponentMove: Move.Paper,
+      outcome: Outcome.PlayerLoses,
+    },
+    {
+      playerMove: Move.Paper,
+      opponentMove: Move.Rock,
+      outcome: Outcome.PlayerWins,
+    },
+    {
+      playerMove: Move.Paper,
+      opponentMove: Move.Scissors,
+      outcome: Outcome.PlayerLoses,
+    }
+  );
   return {
     play(playerMove: Move, opponentMove: Move) {
-      if (opponentMove === Move.Scissors) {
-        return Outcome.PlayerLoses;
-      }
-      return Outcome.PlayerWins;
+      const result = scenarios.find(
+        (scenario) =>
+          scenario.playerMove === playerMove &&
+          scenario.opponentMove === opponentMove
+      );
+
+      return result ? result.outcome : Outcome.Tie;
     },
   };
 }
